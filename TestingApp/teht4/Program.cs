bdee1f620ea61f7268e8a785ae8b19ea0f453c4c
K2218 <K2218@student.jamk.fi>
2016-01-20 09:42:00 +0200

    New Project

---
+++ TestingApp/teht4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give age:");
+            string line = Console.ReadLine(); //"50"
+            int age = int.Parse(line); //50
+            if (age < 18)
+            {
+                Console.WriteLine("You are under-age");
+            }
+            else if (age >= 18 && age <= 65)
+            {
+                Console.WriteLine("You are an adult");
+            }
+            else
+            {
+                Console.WriteLine("You are a senior");
+            }
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+        }
+    }
+}
